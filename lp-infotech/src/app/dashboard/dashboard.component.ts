3709ab6a6b0850e94b6ed566330839231bcb3a82
Hardeep2401 <hardeep.singh@lpinfotech.com>
2023-03-01 19:19:28 +0530

    update edit form

--- lp-infotech/src/app/dashboard/dashboard.component.ts
+++ lp-infotech/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Injectable } from '@angular/core';
 
-
-
 @Injectable({
   providedIn: 'root',
 })
@@ -13,15 +11,20 @@ import { Component, Injectable } from '@angular/core';
 })
 export class DashboardComponent {
   url = 'http://localhost:3000/user';
-  
-  tableData: any;
+  userForm: any;
+  tableData: any=[];
   constructor(private http: HttpClient) {}
 
   ngOnInit(value: any) {
     this.http
-      .get<any>('http://localhost:3000/user',value)
+      .get<any>('http://localhost:3000/user', value)
       .subscribe((response) => {
-       this.tableData=response;
+        this.tableData = response;
       });
   }
+  onEdit(tableData:any) {
+   console.log(tableData)
+  }
+ 
+
 }
